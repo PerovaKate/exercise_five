155cf1e4e0ad3dd4819f40d5d36925e288d45581
PerovaKate <85638736+PerovaKate@users.noreply.github.com>
2021-06-17 17:19:59 +0300

    Initial commit

---
+++ five.py
@@ -0,0 +1,7 @@
+string = 'Hello Python'
+print(string[3])
+print(str('Hello Python')[3])
+print(string[0:2])
+print(str('Hello Python')[0:2])
+print((str('Hello Python')[6]) + str('a') + (str('Hello Python')[8:10]))
+
